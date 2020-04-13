00c3bede2854dc239bb6eed271070832ca851ee3
marchuk.nikolay <marchuk.nikolaay@gmail.com>
2020-04-13 09:50:11 +0300

    Init f-strings and a simple if/else task

---
+++ solutions/fstring_and_simple_if_else.py
@@ -0,0 +1,17 @@
+"""
+Bite 101. f-strings and a simple if/else
+
+In this Bite we'll get you to calculate whether a person is able to drive or not based on their minimum age which is
+stored in the constant MIN_DRIVING_AGE.
+
+Print (not return) name is allowed to drive or name is not allowed to drive (note there is no period (.) at the end),
+based on the age being equal or greater than MIN_DRIVING_AGE. """
+
+
+MIN_DRIVING_AGE = 18
+
+
+def allowed_driving(name, age):
+    """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
+       checking the passed in age against the MIN_DRIVING_AGE constant"""
+    pass
