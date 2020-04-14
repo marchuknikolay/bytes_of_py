dd2088b190ba716f9c6de7dd588d034ae6d8b0b2
marchuk.nikolay <marchuk.nikolaay@gmail.com>
2020-04-14 13:06:31 +0300

    Completed Bite 238. Write tests for Fibonacci

--- byte238/test_fibonacci.py
+++ byte238/test_fibonacci.py
@@ -1,2 +1,19 @@
+import pytest
+
+from byte238.fibonacci import fib
+
+
+def test_negative_value():
+    with pytest.raises(ValueError):
+        fib(-1)
+
+
+def test_zero_one():
+    assert fib(0) == 0
+    assert fib(1) == 1
+
+
 def test_fib():
-    assert False
+    assert fib(2) == 1
+    assert fib(3) == 2
+    assert fib(10) == 55
