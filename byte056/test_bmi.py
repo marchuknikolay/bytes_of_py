e8881f5c5359107170afa1f49d9fe1dadc43ccc0
marchuk.nikolay <marchuk.nikolaay@gmail.com>
2020-04-14 20:43:45 +0300

    Inited 56, 64, 66, 67 bites

---
+++ byte056/test_bmi.py
@@ -0,0 +1,54 @@
+import pytest
+
+from byte056.bmi import handle_args, create_parser
+
+
+@pytest.fixture
+def parser():
+    return create_parser()
+
+
+def test_no_args_exits(parser):
+    # parser.parse_args should raise the exception but in case
+    # you raised it explicitly further down the stack let's check
+    # if handle_args raises it (same applies to next test)
+    with pytest.raises(SystemExit):
+        handle_args()
+
+
+def test_help_flag_exits(parser):
+    with pytest.raises(SystemExit):
+        args = parser.parse_args(['-h'])
+        handle_args(args)
+
+
+def test_only_width_exits(parser):
+    with pytest.raises(SystemExit):
+        args = parser.parse_args(['-w', '80'])
+        handle_args(args)
+
+
+def test_only_length_exits(parser):
+    with pytest.raises(SystemExit):
+        args = parser.parse_args(['-l', '187'])
+        handle_args(args)
+
+
+def test_two_arg(parser, capfd):
+    args = parser.parse_args(['-w', '80', '-l', '187'])
+    handle_args(args)
+    output = capfd.readouterr()[0]
+    assert "Your BMI is: 22.88" in output
+
+
+def test_two_arg_reversed_order(parser, capfd):
+    args = parser.parse_args(['-l', '187', '-w', '80'])
+    handle_args(args)
+    output = capfd.readouterr()[0]
+    assert "Your BMI is: 22.88" in output
+
+
+def test_different_args(parser, capfd):
+    args = parser.parse_args(['-l', '200', '-w', '100'])
+    handle_args(args)
+    output = capfd.readouterr()[0]
