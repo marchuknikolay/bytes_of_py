650702e6448c3669984f931d390ec692416863a2
marchuk.nikolay <marchuk.nikolaay@gmail.com>
2020-04-15 23:17:25 +0300

    Completed 21 bite

--- byte021/cars.py
+++ byte021/cars.py
@@ -27,22 +27,26 @@ cars = {
 def get_all_jeeps(cars=cars):
     """return a comma  + space (', ') separated string of jeep models
        (original order)"""
-    pass
+
+    return ', '.join(cars['Jeep'])
 
 
 def get_first_model_each_manufacturer(cars=cars):
     """return a list of matching models (original ordering)"""
-    pass
+
+    return [models[0] for models in cars.values()]
 
 
 def get_all_matching_models(cars=cars, grep='trail'):
     """return a list of all models containing the case insensitive
        'grep' string which defaults to 'trail' for this exercise,
        sort the resulting sequence alphabetically"""
-    pass
+
+    return sorted([model for manufacturer, models in cars.items() for model in models if grep.lower() in model.lower()])
 
 
 def sort_car_models(cars=cars):
     """return a copy of the cars dict with the car models (values)
        sorted alphabetically"""
-    pass
+
+    return {manufacturer: sorted(models) for manufacturer, models in cars.items()}
