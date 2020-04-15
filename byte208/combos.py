6bec87d87f6941e05ffcde33429ab00f25681632
marchuk.nikolay <marchuk.nikolaay@gmail.com>
2020-04-15 18:45:02 +0300

    Inited 208, 209, 214 bites

---
+++ byte208/combos.py
@@ -0,0 +1,15 @@
+"""
+Bite 208. Find the number pairs summing up N
+
+In this Bite you complete find_number_pairs which receives a list of numbers and returns all the pairs that sum up N
+(default=10). Return this list of tuples from the function.
+
+So in the most basic example if we pass in [2, 3, 5, 4, 6] it returns [(4, 6)] and if we give it [9, 1, 3, 8,
+7] it returns [(9, 1), (3, 7)]. The tests check more scenarios (floats, other values of N, negative numbers).
+
+Have fun and keep calm and code in Python
+"""
+
+
+def find_number_pairs(numbers, N=10):
+    pass
---
+++ byte209/docstring.py
@@ -0,0 +1,33 @@
+"""
+Bite 209. Write a Sphinx docstring
+
+Your team uses Sphinx and wants you to comply with its standards for docstrings. As per the Sphinx-RTD-Tutorial a
+typical Sphinx docstring has the following format:
+
+""[Summary]
+
+:param [ParamName]: [ParamDescription], defaults to [DefaultParamVal]
+:type [ParamName]: [ParamType](, optional)
+...
+:raises [ErrorType]: [ErrorDescription]
+...
+:return: [ReturnDescription]
+:rtype: [ReturnType]
+""
+
+Based on this format and using the literal strings in italics below, write a docstring for sum_numbers that:
+- has Sums numbers for the Summary part,
+- receives numbers: a list of numbers (type: list),
+- raises a TypeError: if not all numeric values passed in,
+- and returns sum of numbers (type: int)
+
+There's no need to implement the function (leave the pass in) as this is about writing the proper docstring.
+
+Note:We will be adding annotations to this function in the next Bite so you will need to complete this Bite to unlock
+the next.
+"""
+
+
+def sum_numbers(numbers):
+    """Write your docstring here ..."""
+    pass
