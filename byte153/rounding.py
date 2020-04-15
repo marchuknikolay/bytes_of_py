5f988fd3a5f9218638e19b2c2dbd94fa9ffb9416
marchuk.nikolay <marchuk.nikolaay@gmail.com>
2020-04-15 14:08:07 +0300

    Inited 153, 161, 165, 167 bites

---
+++ byte153/rounding.py
@@ -0,0 +1,19 @@
+"""
+Bite 153. Round a sequence of numbers
+
+It's time to get mathematical! In this Bite we ask that you complete the round_up_or_down function that receives a
+transactions list of floats and an optional up argument.
+
+If up is True (default) you round them up to the nearest full integer, if it is False, you round down to the nearest
+full integer. Return a new list with the rounded int values.
+
+Use whatever method you see fit, good luck!
+"""
+
+
+def round_up_or_down(transactions, up=True):
+    """Round the list of transactions passed in.
+       If up=True (default) ronud up, else round down.
+       Return a new list of rounded values
+    """
+    pass
---
+++ byte161/tree.py
@@ -0,0 +1,51 @@
+"""
+Bite 161. Count the number of files and directories
+
+Complete count_dirs_and_files traversing the passed in directory path.
+
+Return a tuple of (number_of_directories, number_of_files)
+
+Let's use the tree command to show an example:
+
+$ mkdir -p project/a/1/I project/a/1/II project/a/2 project/b/1/I
+tree project/
+project/
+├── a
+│   ├── 1
+│   │   ├── I
+│   │   └── II
+│   └── 2
+└── b
+    └── 1
+        └── I
+
+8 directories, 0 files
+
+Your solution should match these counts:
+
+$ python
+>>> from tree import count_dirs_and_files
+>>> count_dirs_and_files('project')
+(8, 0)
+
+Let's add two files:
+
+$ touch project/a/1/I/bob
+$ touch project/a/2/julian
+$ python
+>>> from tree import count_dirs_and_files
+>>> count_dirs_and_files('project')
+(8, 2)
+
+Good luck and have fun!
+"""
+
+
+import os
+
+
+def count_dirs_and_files(directory='.'):
+    """Count the amount of of directories and files in passed in "directory" arg.
+       Return a tuple of (number_of_directories, number_of_files)
+    """
+    pass
