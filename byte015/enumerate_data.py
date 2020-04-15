1b8835462f1c430fd625e85150e6e72f4697eb7b
marchuk.nikolay <marchuk.nikolaay@gmail.com>
2020-04-15 21:13:06 +0300

    Completed 15 bite

--- byte015/enumerate_data.py
+++ byte015/enumerate_data.py
@@ -32,4 +32,6 @@ def enumerate_names_countries():
        4. Dante      Argentina
        5. Martin     USA
        6. Rodolfo    Mexico"""
-    pass
+
+    for i, (name, country) in enumerate(zip(names, countries), start=1):
+        print(f'{i}. {name:<10} {country}', end='')
