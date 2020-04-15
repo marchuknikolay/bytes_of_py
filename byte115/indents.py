f7fe20125f5fad55686ba5eb41a4325077420601
marchuk.nikolay <marchuk.nikolaay@gmail.com>
2020-04-15 12:37:41 +0300

    Inited 115, 117, 128, 130 bites

---
+++ byte115/indents.py
@@ -0,0 +1,17 @@
+"""
+Bite 115. Count leading spaces
+
+A small but interesting Bite: given a string with leading indent spacing, calculate the amount of space (literal
+space, not tab or newline) characters:
+- 'string  ' -> 0 (we only care about leading spacing)
+- '  string' -> 2
+- '    string' -> 4
+- etc...
+
+This can be done in many ways so once done we encourage you to go into the Bite forum (Discussion tab that appears upon solving the Bite) to discuss your solution with fellow Pythonistas ... Have fun!
+"""
+
+
+def count_indents(text):
+    """Takes a string and counts leading white spaces, return int count"""
+    pass
