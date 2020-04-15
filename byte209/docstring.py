b608230082b2ed080c7ad85e3056d17e29101452
marchuk.nikolay <marchuk.nikolaay@gmail.com>
2020-04-15 18:52:39 +0300

    Completed 209 bite

--- byte209/docstring.py
+++ byte209/docstring.py
@@ -29,5 +29,14 @@ the next.
 
 
 def sum_numbers(numbers):
-    """Write your docstring here ..."""
+    """Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+
+    :raises TypeError: if not all numeric values passed in
+
+    :return: sum of numbers
+    :rtype: int
+    """
     pass
--- byte209/test_docstring.py
+++ byte209/test_docstring.py
@@ -1,1 +1,17 @@
-# The tests for this Bite are hidden as part of the challenge. They will be shown upon resolving the Bite.
+import re
+
+from byte209.docstring import sum_numbers
+
+
+def test_sum_numbers():
+    doc = f'\n{sum_numbers.__doc__.strip()}'
+
+    # for some lines allow variable content after colon
+    for line in ('Sums numbers',
+                 '    :param numbers: \S.*?\n',
+                 '    :type numbers: list',
+                 '    :raises TypeError: \S.*?\n',
+                 '    :return: \S.*?\n',
+                 '    :rtype: int'):
+        # newline to test proper indenting
+        assert re.search(rf'\n{line}', doc)
