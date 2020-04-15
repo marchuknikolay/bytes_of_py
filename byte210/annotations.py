d18f1428f6b9aa654d4175b5a789906444ca16c2
marchuk.nikolay <marchuk.nikolaay@gmail.com>
2020-04-15 19:02:27 +0300

    Inited 210, 215, 218, 225 bites

---
+++ byte210/annotations.py
@@ -0,0 +1,21 @@
+"""
+Bite 210. Add Type Annotations
+
+Continuing from where we left off in Bite 209, use the docstring you wrote in last Bite to add type annotations to
+the sum_numbers function.
+
+Note that you will need to import one of the types from the typing module. Again no need to implement the function.
+It's about typing here. Have fun!
+"""
+
+
+def sum_numbers(numbers):
+    """Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+    :raises TypeError: if not all numeric values passed in
+    :return: sum of numbers
+    :rtype: int
+    """
+    pass
---
+++ byte225/convert_chars.py
@@ -0,0 +1,26 @@
+"""
+Bite 225. Swap case PyBites characters
+
+In this Bite you will swap case all pybites characters (both lower- and upper case) for a given text.
+
+Not much more as for our instruction, just complete convert_pybites_chars which should work like this:
+
+>>> from convert_chars import convert_pybites_chars
+>>> text = "Today we added TWO NEW Bites to our Platform, exciting!"
+>>> convert_pybites_chars(text)
+'todaY wE addEd tWO NeW bITES To our plaTform, ExcITIng!'
+
+(Note that all converted chars are in the string pybites)
+
+Have fun and code more Python!
+
+"""
+
+
+PYBITES = "pybites"
+
+
+def convert_pybites_chars(text):
+    """Swap case all characters in the word pybites for the given text.
+       Return the resulting string."""
+    pass
