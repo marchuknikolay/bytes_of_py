2ffb23f1206f2371699678e7febfcf25cccd5832
marchuk.nikolay <marchuk.nikolaay@gmail.com>
2020-04-16 12:28:32 +0300

    Completed 32 bite

--- byte032/inventor.py
+++ byte032/inventor.py
@@ -12,10 +12,12 @@ Regardless we want you to think about Python's mutability. Have fun!
 """
 
 
+from copy import deepcopy
+
 items = [{'id': 1, 'name': 'laptop', 'value': 1000},
          {'id': 2, 'name': 'chair', 'value': 300},
          {'id': 3, 'name': 'book', 'value': 20}]
 
 
 def duplicate_items(items):
-    return items[:]
+    return deepcopy(items)
