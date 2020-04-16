2735ff24638240999a45d7c986b75a547a685e71
marchuk.nikolay <marchuk.nikolaay@gmail.com>
2020-04-16 12:38:58 +0300

    Completed 37 bite

--- byte037/countdown.py
+++ byte037/countdown.py
@@ -19,4 +19,9 @@ def countdown_for(start=10):
 
 
 def countdown_recursive(start=10):
-    pass
+    if start <= 0:
+        print('time is up')
+        return
+
+    print(start)
+    countdown_recursive(start - 1)
